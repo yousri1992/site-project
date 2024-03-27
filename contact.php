d0d8606c607e5bec2f16b90b62a21b504ddee9b3
yousri <arbache1992@gmail.com>
2024-03-27 22:28:21 +0100

    ajouter la page contact avec une modefication dans le css

---
+++ contact.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>contact stalgerie</title>
+    <script src="app.js"></script>
+    <script>
+       
+    </script>
+    <style>
+        
+    </style>
+</head>
+<body>
+<form method="post">
+    <div class="mainC">
+        <div class="navbar">
+        <div class="icon">  
+                <h2 class="logo">STALGERIE</h2>    
+            </div>
+                   <div class="menu">
+            
+                        <ul >
+                        <Li class="active"><a href="/GL/index.php">HOME</a></Li>
+                        <Li class="active"><a href="/GL/about.php">ABOUT</a></Li>
+                        <Li class="active"><a href="/GL/contact.php">CONTACT</a></Li>
+                        <Li class="active"><a href="/GL/help.php">HELP</a></Li>
+                        </ul>
+                
+                    </div>
+                    
+        </div>
+                    <div class="content">
+                    <h1>LES STAGES</h1><br><span>DISPONIBLE</span>
+                           <p class="paragraphe"> arbache1992@gmail.com</p>
+                             
+                    </div>
+                    
+        
+        
+    </div>
+    
+
+    
+    
+    </form>
+  
+</body>
+</html>
