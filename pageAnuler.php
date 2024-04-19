79a9aa4bdba32fb0ad0c66491d320f532d49d2be
yousri <arbache1992@gmail.com>
2024-04-19 02:19:48 +0100

    la confirmation des demande ou l anulation

---
+++ pageAnuler.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+
+// Vérifier si l'identifiant de la demande est présent dans la requête GET et si l'utilisateur est connecté
+if (isset($_GET['id_demande']) && isset($_SESSION['ID_entreprise'])) {
+    $id_demande = $_GET['id_demande'];
+    $matricule_entreprise = $_SESSION['ID_entreprise'];
+
+    // Vérifier si la confirmation a été confirmée
+    if (isset($_GET['confirm']) && $_GET['confirm'] === 'true') {
+        // Connexion à la base de données
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "bd_stalgerie";
+        $connection = new mysqli($servername, $username, $password, $database);
+
+        // Vérifier la connexion à la base de données
+        if ($connection->connect_error) {
+            die("La connexion a échoué : " . $connection->connect_error);
+        }
+
+            // Préparer la requête pour mettre à jour l'état de la demande
+$sql_update = "UPDATE tab_demande SET etat_demande = 2 WHERE id_demande = ?";
+$stmt = $connection->prepare($sql_update);
+
+// Vérifier si la préparation de la requête a réussi
+if ($stmt) {
+    // Liaison des paramètres et exécution de la requête
+    $stmt->bind_param("i", $id_demande); 
+    $stmt->execute();
+    $stmt->close();
+} else {
+    echo "Erreur lors de la préparation de la requête.";
+}
+
+        // Fermeture de la connexion à la base de données
+        $connection->close();
+        
+        // Redirection vers la page d'affichage des demandes après la confirmation
+        header("location: /GLT/laCause.php");
+        exit;
+    } else {
+        // Afficher un message de confirmation
+        echo "
+        <script>
+        var confirmed = confirm('Êtes-vous sûr de vouloir ajouter cette demande ?');
+        if (confirmed) {
+            window.location.href = '/GLT/pageAnuler.php?id_demande=$id_demande&confirm=true';
+        } else {
+            window.location.href = '/GLT/index.php';
+        }
+        </script>
+        ";
+    }
+}
+?>
